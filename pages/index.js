914595cf33642e3afcf06c92c8e17ad85d1c0df3
idncod <idncod@protonmail.com>
2023-11-05 02:23:00 +0000

    upd_communication

--- pages/index.js
+++ pages/index.js
@@ -47,7 +47,7 @@ export default function Home() {
                 </a> */}
                 <a
                   className="bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white px-4 py-2 border-none rounded-md ml-8"
-                  href="tel:+44(0)7565475880"
+                  href="mailto:idncod@protonmail.com"
                 >
                   Call me now!
                 </a>
